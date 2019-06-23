53f8adf085da203efad1d405cf1331ead4d829a8
zsxs2 <42575254+zsxs2@users.noreply.github.com>
2019-06-23 00:38:56 +0000

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,93 @@
+var feed_data = {  
+    "domain":  { "name": "当前域名", 
+                 "value": [ "WkROMWVXMDRaR2MzT0d4eU9UVXVZMnh2ZFdSbWNtOXVkQzV1WlhRPQ==", "WkRKdE1uWmlkV3Q0YjJWMWJHZ3VZMnh2ZFdSbWNtOXVkQzV1WlhRPQ==", "WkhRM2EybGpkVGxzWmpkcGFDNWpiRzkxWkdaeWIyNTBMbTVsZEE9PQ==","WkRFMlozcG5PR3B5TVdnMU56SXVZMnh2ZFdSbWNtOXVkQzV1WlhRPQ==", "WkROMU9YUTBaekZzTkcwM2JHRXVZMnh2ZFdSbWNtOXVkQzV1WlhRPQ==", "WkRGak1qY3hZbkJvYkRGaU5tSXVZMnh2ZFdSbWNtOXVkQzV1WlhRPQ=="],
+                 "fq": "WkROcGJHTnlNVFV3YWpBNE5Yb3VZMnh2ZFdSbWNtOXVkQzV1WlhRPQ=="
+               },
+               
+   "shorturl": { "name": "短网址", 
+                 "value": [ "ZEM1amJpOUJhWEEwYVhSQ05RPT0=", "ZEM1amJpOUJhWEEwYVhSUlNBPT0=", "ZEM1amJpOUJhWEEwYVhSNGRBPT0=","ZEM1amJpOUJhWEEwYVhSQ05RPT0=", "ZEM1amJpOUJhWEEwYVhSRVZBPT0=", "ZEM1amJpOUJhWEEwYVhSUlNBPT0="]
+               }, 
+   
+  "ogateurl": { "name": "ogate",
+                 "value": [ "ZEM1amJpOUJhWEEwYVhSUldRPT0=", "ZEM1amJpOUJhWEEwYVhSQ1l3PT0=", "ZEM1amJpOUJhWEEwYVhSRVNBPT0=","ZEM1amJpOUJhWEEwYVhSNFZ3PT0=", "ZEM1amJpOUJhWEEwYVdOb1p3PT0=", "ZEM1amJpOUJhWEEwYVhSRVNBPT0="]
+               }, 
+  "fo04url": { "name": "fo04",
+                 "value": [ "TVM0eE5qTXVOakV1TkRRPQ==", "TVRFNExqRTJOeTR4TVRBdU1UazI=", "TmpFdU1qSTRMakl3T1M0eE1BPT0="]
+               }, 
+  "longacting": { "name": "longdomain",
+                 "value": [ "TWpjdU1UQTFMak0zTGpJek1nPT0=", "TmpFdU1qSTRMakl3T1M0eU1EWT0=", "Y25scGFtcGxhMlV1WjNWcGJIVXVjM0JoWTJVPQ==","ZVdkeGFuVmhiaTU2YjJwaExuUnZjQT09", "YkhCdGVIVnBMbWh2Ym1kNWRXRnVMbk53WVdObA==", "Ym5abGMyY3VlR0ZyWVM1MGIzQT0="]
+               },
+  "ogColl": { "name": "网门",
+                 "value": [ "ZEM1amJpOUZlbkZxUlVndw==", "ZEM1amJpOUZSMWhyYlVWbQ==", "Ym5wcGNtZDBheTV4ZW5oblptZ3VaMkV2TmpnPQ=="]
+               }, 
 
+    "article": { "name": "最新文章", 
+                 "value": [
+                           { "title": "世纪骗局 天安门自焚伪案揭秘", "url": "/70" },
+                           { "title": "佛法修炼你知道多少？", "url": "/71" },
+                           { "title": "宇宙本身就是一个生命", "url": "/72" },
+                           { "title": "更多文章...", "url": "/news.php" }
+                          ]
+               },
+               
+   "video":  { "name": "精彩视频",
+                 "value": [
+                           { "title": "新唐人直播", "url": "/tEXS"},
+                           { "title": "新唐人亚太台", "url": "/v.php?id=ntd-mlt"},
+                           { "title": "天国乐团", "url": "/video/play/1094.html" },
+                           { "title": "共产主义的终极目的", "url": "/video/play/1035.html" },                   
+                           { "title": "更多影音...", "url": "/video/" }
+                          ]
+               },
+               
+    "software":{ "name": "翻墙软件",
+                 "value": [ 
+                            { "title": "下载一键翻墙客户端", "url": "/12" },
+                            { "title": "自由门翻墙软件", "url": "/13" },
+                            { "title": "无界浏览下载", "url": "/11" },
+                            { "title": "手机版翻墙软件", "url": "/77" },
+                            { "title": "更多软件...", "url": "/78" }
+                          ]
+               },
+               
+    "website": { "name": "网址导航",
+                 "value": [ 
+                            { "title": "法轮大法明慧网", "url": "/7/" },
+                            { "title": "动态网", "url": "/1/" },
+                            { "title": "新唐人电视台", "url": "/3/" },
+                            { "title": "更多网址...", "url": "/0" }
+                          ]
+               },
+ 
+     "others": { "name": "神州营救",
+                 "value": [ 
+                            { "title": "紧急营救", "url": "/19/" },
+                            { "title": "黑龙江营救", "url": "/?yvq=eWo3MjauymxVZ3NwB3qudHcVc2vhcmNoL2xhymvsLYvFNSU4NSvCMYvFNivCMYvbOCvFOSvCqiU5MSvFOSvCRSU5OSvFNivCMSU5RivFOCvCRivbqivFNSvbRSvCMw" },
+                            { "title": "山东营救", "url": "/?yvq=eWo3MjauymxVZ3NwB3qudHcVc2vhcmNoL2xhymvsLYvFNSU4NSvCMYvFNivCMYvbOCvFNSvCMSvCMSvFNCvCOCU5qYvFOCvCRivbqivFNSvbRSvCMw" },
+                            { "title": "追 查 令---被追查人 吕红军", "url": "/ekCX" },
+                            { "title": "更多内容...", "url": "/faq.php" }
+                          ]
+               }, 
+               
+    "contact": { "name": "联系我们",
+                 "url": "/78",
+                 "email": "YzJobGJucG9iM1Y2YUdWdVoyUmhiMEJuYldGcGJDNWpiMjA9"
+               },       
+               
+  "copyright": { "txt": "&copy; 2015-2018 &#x795E;&#x5DDE;&#x6B63;&#x9053;",
+                 "domain": "ZDNkM0xuTm9aVzU2YUc5MWVtaGxibWRrWVc4dWIzSm4="
+               },                 
+               
+        "tui": { "url": "/10",
+                 "value": "c2hlbnpob3V6aGVuZ2Rhby5vcmc="
+               },
+               
+"version": { "ver": "4.1",
+                 "url": "https://git.io/fxNjC",
+                 "sha1": "0D35F7294986F2DB2FEE689AD470693791671551",
+                 "sha512": "f6f49be558304cffe23a8d2efb154825e92de3d25a7142d9d092eda0fa1d09cee4284a8f7fe80d3abec34d119113b7f9a38d5f2a3c30ec26e5a1a249b10517b0
+",
+                 "time": "1561250333"
+               } 
+     
+ }     
